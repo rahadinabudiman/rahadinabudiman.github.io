662b153c9aa3097629b3df6ad20a8d0475a586d3
DESKTOP-EM7KJIP\R4HA <erbees.50000@gmail.com>
2021-11-18 23:08:20 +0700

    update index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
-var typed = new Typed('#typed', {
-      strings:[
-            'My name is Rahadina Budiman Sundara.',
-            '21 Years Old.',
-            'I live in Indonesia.',
-            'Reading, listen Music, and wrote some code was my hobby.',
-            'Check the buttons on the side if you want to visit my social media.',
-            'Cheers!',
+document.addEventListener('DOMContentLoaded', function () {
+      var typed = new Typed('#typed', {
+            strings: [
+                  'My name is Rahadina Budiman Sundara.',
+                  '21 Years Old.',
+                  'I live in Indonesia.',
+                  'Reading, listen Music, and wrote some code was my hobby.',
+                  'Check the buttons on the side if you want to visit my social media.',
+                  'Cheers!',
             ],
-      typeSpeed: 45,
-      backSpeed: 80,
-      fadeOut: true,
-      loop: true
-});
+            typeSpeed: 45,
+            backSpeed: 80,
+            fadeOut: true,
+            loop: true
+      });
 });
 
 // active header bar
 const lokasi_sekarang = location.href;
 const menu = document.querySelectorAll('a');
 const menu_length = menu.length;
-for(let i=0;i<menu_length;i++){
-      if(menu[i].href == lokasi_sekarang){
-            menu[i].className ="active"
+for (let i = 0; i < menu_length; i++) {
+      if (menu[i].href == lokasi_sekarang) {
+            menu[i].className = "active"
       }
 }
+
+// validation form
+document.addEventListener("DOMContentLoaded", function () {
+      document.querySelectorAll(".form__input").forEach(function (input) {
+            input.addEventListener("input", function () {
+                  input.className = input.className.replace(/form__input--error ?/, "");
+            });
+      });
+});
